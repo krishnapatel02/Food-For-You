de2be64a3bb4fa79df43be605ac06b24155aab85
katherinesmirnov <54304561+katherinesmirnov@users.noreply.github.com>
2023-03-05 23:00:30 -0800

    Update staffUI.py

--- staffUI.py
+++ staffUI.py
@@ -5,37 +5,68 @@ from tkinter import messagebox
 
 #https://www.google.com/search?q=create+theme+tkinter+python&rlz=1C1VDKB_enUS1034US1034&sxsrf=AJOqlzVyUBHWRfGeC6eRK0zbFZLyOMlJmw%3A1678037292298&ei=LNEEZN7tEY660PEPkbOW6AE&ved=0ahUKEwjes-iFqMX9AhUOHTQIHZGZBR0Q4dUDCBA&uact=5&oq=create+theme+tkinter+python&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIICCEQoAEQwwQyCAghEKABEMMEOgoIABBHENYEELADOgcIIxCwAhAnOggIABAIEAcQHjoICAAQCBAeEA06BQgAEIYDOgoIIRCgARDDBBAKSgQIQRgAUNoDWMMJYO0KaAFwAXgAgAF3iAH5BJIBAzUuMpgBAKABAcgBCMABAQ&sclient=gws-wiz-serp#fpstate=ive&vld=cid:55497400,vid:fOVmMiyezMU
 
+def fetchLocations():
+    return ["123 ferry street"]
+    # connection = connectToDatabase("jerryp", "111", "ix-dev.cs.uoregon.edu", 3624, "foodforyou")
+    # cursor = connection.cursor()
+    # cursor.execute("SELECT fb.Location from food_bank fb")
+    # for row in cursor:
+    #     for col in row:
+    #         locations.append(col)
+    # locations.append(None)
+    # return locations
+
 class UpdateItem:
-    def __init__(self, parent, window, item, quantity, units, location):
-        self.root = root
-        self.root.title("Updating")
-        self.root.geometry("500x200")
-        self.locations = parent.locations
+    def __init__(self, screen, item, quantity, units, location):
+        self.screen = screen
+        self.screen.title("Updating")
+        self.screen.geometry("500x200")
+        self.locations = fetchLocations()
 
-        style = ttk.Style(root)
-        style.theme_use("clam")
+        self.item_to_update = StringVar()
+        self.quantity_to_update = IntVar()
+        self.loc_to_update = StringVar()
+        self.units_to_update = StringVar()
 
-        frame = Frame(root)
-        frame.place(x=20, y=50, width=500, height=745)
 
-        ttk.Label(frame, text="item").place(x=25, y=25)
-        iteminput = ttk.Entry(frame, width=10, textvariable=self.item_to_update)
+        screen.configure(background='white')
+
+        style = ttk.Style(screen)
+        style.theme_create("Custom")
+        #style.wm_attributes('-transparentcolor', '#ab23ff')
+        style.configure("TLabel", font=("arial", 11), background ="#fff")
+        style.configure("TButton", font=("arial", 11), background="#fff")
+        style.configure("TCheckbutton", font=("arial", 11), background ="#fff")
+        style.configure("TEntry", font=("arial", 11))
+
+
+        ttk.Label(screen, text="item").place(x=25, y=25)
+        iteminput = ttk.Entry(screen, width=10, textvariable=self.item_to_update)
+        iteminput.insert(0, item)
         #iteminput.grid(row=1, column=0, sticky='w', padx=10, pady=11)
         iteminput.place(x=25, y=50)
 
-        ttk.Label(frame, text="quantity").place(x=150, y=25)
-        quantityinput = ttk.Entry(frame, width=10, textvariable=self.quantity_to_update)
+        ttk.Label(screen, text="quantity").place(x=150, y=25)
+        quantityinput = ttk.Entry(screen, width=10, textvariable=self.quantity_to_update)
+        quantityinput.insert(0, quantity)
         #quantityinput.grid(row=1, column=0, sticky='w', padx=10, pady=11)
         quantityinput.place(x=150, y=50)
 
-        Label(frame, text="location").place(x=265, y=25)
-        locationDD = ttk.Combobox(frame, values=self.locations, textvariable=self.loc_to_update)
+        ttk.Label(screen, text="units").place(x=150, y=100)
+        unitsInput = ttk.Entry(screen, width=10, textvariable=self.units_to_update)
+        unitsInput.insert(0, units)
+        unitsInput.place(x=150, y=125)
+
+        ttk.Label(screen, text="location").place(x=265, y=25)
+        locationDD = ttk.Combobox(screen, values=self.locations, textvariable=self.loc_to_update)
+        locationDD.insert(0, location)
         locationDD.place(x=275, y=50)
+
 
         def saveChanges():
 
                         #If one or more required field is empty, show error
-            if (iteminput.get() == "" or quantityinput.get() == "" or locationDD.get()):
+            if (iteminput.get() == "" or quantityinput.get() == "" or locationDD.get() == "" or unitsInput.get() == ""):
                     messagebox.showerror("ERROR", "Please enter the correct data in each catagory")
             #otherwise update all categories
             else:
@@ -51,32 +82,37 @@ class UpdateItem:
                 messagebox.showinfo('Success', f'Course CRN: {crn} is updated in the database')
             """
 
-        submitButton = Button(root, text="Save changes", height=2, width=10, command=saveChanges)
+        submitButton = Button(screen, text="Save changes", height=2, width=10, command=saveChanges)
         submitButton.place(x=400, y=150)
 
+
+
+
 def connectToDatabase(user, password, host, port, database):
+    dbconnect = None
+    counter = 0
+    while dbconnect is None:
+        if (counter >= 10):
+            print("Check connection to internet")
+            exit()
+        try:
+            dbconnect = mysql.connector.connect(
+                host=host,
+                user=user,
+                passwd=password,
+                port=port,
+                database=database)
+            print("Connected")
+        except:
+            print("Connection failed")
             dbconnect = None
-            counter = 0
-            while dbconnect is None:
-                if (counter >= 10):
-                    print("Check connection to internet")
-                    exit()
-                try:
-                    dbconnect = mysql.connector.connect(
-                        host=host,
-                        user=user,
-                        passwd=password,
-                        port=port,
-                        database=database)
-                    print("Connected")
-                except:
-                    print("Connection failed")
-                    dbconnect = None
-                    counter += 1
-            return dbconnect
+            counter += 1
+    return dbconnect
+
 
 connection = connectToDatabase("jerryp", "111", "ix-dev.cs.uoregon.edu", 3624, "foodforyou")
 cursor = connection.cursor()
+
 
 class StaffGUI:
     def __init__(self, root):
@@ -86,17 +122,28 @@ class StaffGUI:
         self.foodItemSearchText = StringVar()
         self.ascSort = BooleanVar()
         self.loc_to_update = StringVar()
-        self.locations = []
+        self.locations = fetchLocations()
         self.bg = PhotoImage(file="backgroundimg1.png")
         Label(root, image=self.bg).place(x=0, y=0)
+        try:
+            self.bg = PhotoImage(file="img\\backgroundimg.png")
+            Label(root, image=self.bg, borderwidth=0, highlightthickness=0).place(x=0, y=0)
+
+            self.trailing_img = PhotoImage(file="img\\trailingIMG.png")
+            for i in range(0, self.screenWidth, self.trailing_img.width()):
+                Label(root, image=self.trailing_img, bg='white').place(x=i, y=480)
+        except:
+            pass
+
+        root.configure(background='white')
 
         style = ttk.Style(root)
         style.theme_create("Custom")
-        #style.wm_attributes('-transparentcolor', '#ab23ff')
-        style.configure("TLabel", font=("arial", 11), background ="#fff")
+        # style.wm_attributes('-transparentcolor', '#ab23ff')
+        style.configure("TLabel", font=("arial", 11), background="#fff")
         style.configure("TButton", font=("arial", 11), background="#fff")
-        style.configure("TCheckbutton", font=("arial", 11), background ="#fff")
-        style.configure("TEntry", font=("arial"), background ="#fff")
+        style.configure("TCheckbutton", font=("arial", 11), background="#fff")
+        style.configure("TEntry", font=("arial", 11))
 
         #
         # style.theme_settings("Custom", {
@@ -110,28 +157,16 @@ class StaffGUI:
         #
         # })
 
-       
-
-        def fetchLocations():
-            cursor.execute("SELECT fb.Location from food_bank fb order by fb.Location asc")
-            self.locations.append(None)
-            for row in cursor:
-                for col in row:
-                    self.locations.append(col)
-
-        fetchLocations()
-
         def fetchData():
             search()
             rows = cursor.fetchall()
-            
-            #Delete the old table and insert each row in the current database to accomplish refresh
+
+            # Delete the old table and insert each row in the current database to accomplish refresh
             if rows != 0:
                 table.delete(*table.get_children())
-            
+
                 for row in rows:
                     table.insert('', END, values=row)
-
 
         def search():
             item = ItemSearch.get()
@@ -168,9 +203,8 @@ class StaffGUI:
                 cursor.execute(
                     f"SELECT fi.Item_name, fi.Quantity, fi.Units, fi.fd_id, fb.Location from food_item fi join food_bank fb using(fb_id)")
 
-
         def update(e):
-            #taken from focus(e) by jerry
+            # taken from focus(e) by jerry
             cursor = table.focus()
             content = table.item(cursor)
             row = content['values']
@@ -179,9 +213,8 @@ class StaffGUI:
             units = row[2]
             location = row[3]
 
-            window = Tk()
-            ob = UpdateItem(window, root, item, quantity, units, location)
-            window.mainloop()
+            screen = Tk()
+            UpdateItem(screen, item, quantity, units, location)
 
         ttk.Label(root, text="Search item").place(x=700, y=110)
 
@@ -190,9 +223,9 @@ class StaffGUI:
         SearchButton = ttk.Button(root, text="Search", width=15, command=fetchData)
         SearchButton.place(x=700, y=400)
 
-        #ttk.Label(root, text="Sort quantity").place(x=700, y=50)
+        # ttk.Label(root, text="Sort quantity").place(x=700, y=50)
         QuantitySortButton = ttk.Checkbutton(root, text="Sort Ascending", width=15, command=fetchData, onvalue=True,
-                                         offvalue=False,  variable=self.ascSort)
+                                             offvalue=False, variable=self.ascSort)
         QuantitySortButton.place(x=700, y=300)
 
         ttk.Label(root, text="Sort by location").place(x=700, y=200)
@@ -204,10 +237,12 @@ class StaffGUI:
 
         viewFrame = Frame(root, bd=5, relief='ridge', bg='wheat')
         viewFrame.place(x=30, y=110, width=600, height=350)
-                                            #800
+        # 800
         xScroll = Scrollbar(viewFrame, orient=HORIZONTAL)
         yScroll = Scrollbar(viewFrame, orient=VERTICAL)
-        table = ttk.Treeview(viewFrame, columns=('item_to_filter', 'quantity_to_filter', 'units', 'fid_to_filter', 'location_to_filter'), xscrollcommand=xScroll.set,
+        table = ttk.Treeview(viewFrame, columns=(
+        'item_to_filter', 'quantity_to_filter', 'units', 'fid_to_filter', 'location_to_filter'),
+                             xscrollcommand=xScroll.set,
                              yscrollcommand=yScroll.set)
 
         table.heading("item_to_filter", text="item")
@@ -228,8 +263,10 @@ class StaffGUI:
         table.pack(fill=BOTH, expand=1)
 
 
-
-
 root = Tk()
 ob = StaffGUI(root)
-root.mainloop()
+
+child = Tk()
+UpdateItem(child, "apples", 1, "oz", "123 ferry street")
+child.mainloop()
+#root.mainloop()
