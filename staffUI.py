953be0e9da2bb3520d5217231a70ed1d9a6e4b46
Krishna Patel <krishna.patel97402@gmail.com>
2023-03-05 17:47:26 -0800

    added SQL queries

--- staffUI.py
+++ staffUI.py
@@ -1,7 +1,39 @@
 import tkinter as t
 from tkinter import ttk
-
+import mysql.connector
 #https://www.google.com/search?q=create+theme+tkinter+python&rlz=1C1VDKB_enUS1034US1034&sxsrf=AJOqlzVyUBHWRfGeC6eRK0zbFZLyOMlJmw%3A1678037292298&ei=LNEEZN7tEY660PEPkbOW6AE&ved=0ahUKEwjes-iFqMX9AhUOHTQIHZGZBR0Q4dUDCBA&uact=5&oq=create+theme+tkinter+python&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIICCEQoAEQwwQyCAghEKABEMMEOgoIABBHENYEELADOgcIIxCwAhAnOggIABAIEAcQHjoICAAQCBAeEA06BQgAEIYDOgoIIRCgARDDBBAKSgQIQRgAUNoDWMMJYO0KaAFwAXgAgAF3iAH5BJIBAzUuMpgBAKABAcgBCMABAQ&sclient=gws-wiz-serp#fpstate=ive&vld=cid:55497400,vid:fOVmMiyezMU
+
+def connectToDatabase(user, password, host, port, database):
+    dbconnect = None
+    counter = 0
+    while dbconnect is None:
+        if(counter >= 10):
+            print("Check connection to internet")
+            exit()
+        try:
+            dbconnect = mysql.connector.connect(
+                host = host,
+                user = user,
+                passwd=password,
+                port = port,
+                database = database)
+            print("Connected")
+        except:
+            print("Connection failed")
+            dbconnect = None
+            counter+=1
+    return dbconnect
+
+
+connection = connectToDatabase("jerryp", "111", "ix-dev.cs.uoregon.edu", 3624, "foodforyou")
+cursor = connection.cursor()
+cursor.execute("SELECT fb.Location from food_bank fb")
+locations = []
+for row in cursor:
+    for col in row:
+        locations.append(col)
+locations.append(None)
+print(locations)
 
 def updateItem():
     def saveChanges():
@@ -70,6 +102,8 @@ Function name: focus()
 Usage: Auto fill the values when a table item is selected
 """
 
+foodItemSearchText = None
+
 
 # def focus(e):
 #     cursor = table.focus()
@@ -88,16 +122,43 @@ Usage: Auto fill the values when a table item is selected
 
 def searchQuery():
     item = ItemSearch.get(1.0, "end-1c")
-    global sortAsc
-    locations = LocationFilter.get()
+    locationBool = True
+    itemBool = True
+    location = LocationFilter.get()
+    ascending = ascSort.get()
+    if(location == "None" or location == ""):
+        locationBool = False
+    if(item.strip() == ""):
+        itemBool = False
+    if(locationBool and itemBool and ascending):
+        cursor.execute(f"SELECT fi.Item_name, fi.Quantity, fi.Units, fi.fd_id, fb.Location from food_item fi join food_bank fb using(fb_id) where fi.Item_name='{item}' and fb.location = '{location}' order by fi.Quantity ASC")
+    elif(locationBool and ascending):
+        cursor.execute(f"SELECT fi.Item_name, fi.Quantity, fi.Units, fi.fd_id, fb.Location from food_item fi join food_bank fb using(fb_id) where fb.location = '{location}' order by fi.Quantity ASC")
+    elif(itemBool and ascending):
+        cursor.execute(f"SELECT fi.Item_name, fi.Quantity, fi.Units, fi.fd_id, fb.Location from food_item fi join food_bank fb using(fb_id) where fi.Item_name='{item}' order by fi.Quantity ASC")
+    elif(itemBool and locationBool):
+        cursor.execute(f"SELECT fi.Item_name, fi.Quantity, fi.Units, fi.fd_id, fb.Location from food_item fi join food_bank fb using(fb_id) where fi.Item_name='{item}' and fb.location = '{location}'")
+    elif(itemBool):
+        cursor.execute(f"SELECT fi.Item_name, fi.Quantity, fi.Units, fi.fd_id, fb.Location from food_item fi join food_bank fb using(fb_id) where fi.Item_name='{item}'")
+    elif(locationBool):
+        cursor.execute(f"SELECT fi.Item_name, fi.Quantity, fi.Units, fi.fd_id, fb.Location from food_item fi join food_bank fb using(fb_id) where fb.location = '{location}'")
+    elif(ascending):
+        cursor.execute(f"SELECT fi.Item_name, fi.Quantity, fi.Units, fi.fd_id, fb.Location from food_item fi join food_bank fb using(fb_id) order by fi.quantity ASC")
+    else:
+        cursor.execute(f"SELECT fi.Item_name, fi.Quantity, fi.Units, fi.fd_id, fb.Location from food_item fi join food_bank fb using(fb_id)")
+    result = cursor.fetchall()
+    for line in result:
+        print(line)
+        
 
     ##database stuff
 
 
+
+
 mainScreen = t.Tk()
 mainScreen.geometry("1000x500")
-sortAsc = t.IntVar()
-
+ascSort = t.BooleanVar()
 # UpdateItemButton = t.Button(mainScreen,
 #     text="Update Item",
 #     width = 20,
@@ -108,6 +169,7 @@ sortAsc = t.IntVar()
 ItemSearch = t.Text(mainScreen,
         height=2,
         width=15,
+        
 )
 
 SearchButton = t.Button(mainScreen,
@@ -130,8 +192,9 @@ QuantitySortButton = t.Checkbutton(mainScreen,
     text="Sort Ascending",
     width = 15,
     height = 1,
-    variable = sortAsc,
-    command=searchQuery
+    variable = ascSort,
+    onvalue=True,
+    offvalue=False,
 )
 
 LocationLabel = t.Label(mainScreen,
@@ -139,8 +202,9 @@ LocationLabel = t.Label(mainScreen,
 )
 
 LocationFilter = ttk.Combobox(mainScreen,
-    values=["abc"]
+    values=locations
 )
+
 
 QuantitySortLabel.place(x=700, y=50)
 QuantitySortButton.place(x=700, y=100)
@@ -154,3 +218,4 @@ SearchButton.place(x=725, y=400)
 
 
 mainScreen.mainloop()
+connection.close()
